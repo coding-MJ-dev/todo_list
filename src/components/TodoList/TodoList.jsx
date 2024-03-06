bea701f276f529177398cea687fad5a51df4e8b0
MJ <mwjang628@gmail.com>
2024-03-07 09:45:57 +1100

    Save the todo list as a JASON in the local storage

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css'
 
 export default function TodoList({filter}) {
-  const [todos, setTodos] = useState([
-    {id: '123', text: 'shopping', status: 'active'},
-    {id: '1243', text: 'studying', status: 'active'},
-  ]);
+  const [todos, setTodos] = useState(() => (readTodosFromLocalStorage()));
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) => setTodos(todos.filter((t)=> t.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos]);
 
   const filtered = getFilteredItems(todos, filter);
   return (
@@ -30,6 +31,10 @@ export default function TodoList({filter}) {
   );
 }
 
+function readTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
 
 function getFilteredItems(todos, filter) {
   if (filter === 'all') {
